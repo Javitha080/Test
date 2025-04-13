e38eb74a0699b73361856c6c6fd2027e63f22607
Javitha080 <patroleuro@gmail.com>
2025-04-13 22:45:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function f1(){
-  alert("Helow World!");
-}
-function f2(){
-
-  document.getElementById("btn").style.backgroundColor = "red";
-  
-}
-function f3(){
-
-  document.getElementById("btn").innerHTML = "Read More";
-}
+function start(){
+  alert("HI😁");
+} 
